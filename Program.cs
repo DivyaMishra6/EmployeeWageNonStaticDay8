3c9c9759251054d101c116c466b5116e749b9731
DivyaMishra6 <divyamishra599@gmail.com>
2023-03-22 17:24:15 +0530

    Solving Employee Wages using Object

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using EmployeeWageNonStaticDay8;
+
+namespace EmployeeWagesProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee wage Problem");
+
+            EmployeeCheckUC1 attendance = new EmployeeCheckUC1();
+            attendance.EmployeeAttendence();
+
+            CalculateEmpWagesUC2 wage = new CalculateEmpWagesUC2();
+            wage.EmployeeWages();
+
+            AddPartTimeUC3 parttime = new AddPartTimeUC3();
+            parttime.Wages();
+
+            EmpWagesUsingSwitchCase switchcase = new EmpWagesUsingSwitchCase();
+            switchcase.Case();
+
+            WageForMonthUC5 month = new WageForMonthUC5();
+            month.WageForMonth();
+
+            DaysCalculateUC6 days = new DaysCalculateUC6();
+            days.MaxDaysMaxHoursWage();
+
+            MethodToComputeEmpWageUC7.EmpWageUseCase7();
+
+
+
+
+        }
+    }
+
+
+}
